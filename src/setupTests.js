dd84d97d0ed08472b6558150c50b7b4d343fa15f
William Elliott <wcelliott1991@gmail.com>
2017-12-04 15:55:16 -0500

    passes inital app render test and resolves enzyme packages

---
+++ src/setupTests.js
@@ -0,0 +1,6 @@
+import requestAnimationFrame from './tempPolyFill';
+ 
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+ 
+configure({ adapter: new Adapter(), disableLifecycleMethods: true });
