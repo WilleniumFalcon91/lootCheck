38c64816b31719b3624d166e5e12bca011e370e3
William Elliott <wcelliott1991@gmail.com>
2017-12-05 09:16:39 -0500

    passes tests for storing balance value in localstorage with cookies

--- src/reducers/balance.test.js
+++ src/reducers/balance.test.js
@@ -1,13 +1,23 @@
 import balanceReducer from './balance';
+import balanceReduer2 from './balance';
 import * as constants from '../actions/constants';
 
 describe('balanceReducer', () => {
-    it('sets a balance', () => {
+    describe('when initalizing', () => {
         const balance = 10;
+    
+        it('sets a balance', () => {
+            expect(balanceReducer(undefined, { type: constants.SET_BALANCE, balance }))
+                .toEqual(balance);
+        });
 
-        expect(balanceReducer(undefined, { type: constants.SET_BALANCE, balance }))
-            .toEqual(balance);
-    });
+        describe('then re-initializing', () => {
+            it('reads the balance from cookies', () => {
+                expect(balanceReduer2(undefined, {})).toEqual(balance);
+            });
+        });
+
+    })
 
     it('deposits into the balance', () => {
         const deposit = 10;
