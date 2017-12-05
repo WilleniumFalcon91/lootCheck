bb8b1d7fbd9daf9a6933947ce725ae10ccc581dc
William Elliott <wcelliott1991@gmail.com>
2017-12-04 20:14:35 -0500

    passes withdraw test code to production

--- src/components/Wallet.js
+++ src/components/Wallet.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { deposit } from '../actions/balance';
+import { withdraw } from '../actions/balance';
 
 export class Wallet extends Component {
     constructor(props) {
@@ -14,6 +15,8 @@ export class Wallet extends Component {
 
     deposit = () => this.props.deposit(this.state.balance);
 
+    withdraw = () => this.props.withdraw(this.state.balance);
+
     render() {
         return (
             <div>
@@ -21,6 +24,7 @@ export class Wallet extends Component {
                 <br /> 
                 <input className="input-wallet" onChange={this.updateBalance}/> 
                 <button className="btn-deposit" onClick={this.deposit}>Deposit</button>
+                <button className="btn-withdraw" onClick={this.withdraw}>Withdraw</button>
             </div>
         )
     }
--- src/components/wallet.test.js
+++ src/components/wallet.test.js
@@ -4,7 +4,8 @@ import { Wallet } from './Wallet';
 
 describe('Wallet', () => {
     const mockDeposit = jest.fn();
-    const props = { balance: 20, deposit: mockDeposit };
+    const mockWithdraw = jest.fn();
+    const props = { balance: 20, deposit: mockDeposit, withdraw: mockWithdraw };
     const wallet = shallow(<Wallet {...props} />);
 
     it('renders properly', () => {
@@ -30,12 +31,20 @@ describe('Wallet', () => {
             expect(wallet.state().balance).toEqual(parseInt(userBalance, 10));
         });
 
-        describe('and the user wants to make a desposit', () => {
+        describe('and the user wants to make a deposit', () => {
             beforeEach(() => wallet.find('.btn-deposit').simulate('click'));
 
             it('dispatches the `deposit()` it receives from props with local balance', () => {
                 expect(mockDeposit).toHaveBeenCalledWith(parseInt(userBalance, 10));
-            })
-        })
+            });
+        });
+        
+        describe('and the user wants to make a withdrawl', () => {
+            beforeEach(() => wallet.find('.btn-withdraw').simulate('click'));
+
+            it('dispatches the `withdraw()` it receives from props with local balance', () => {
+                expect(mockWithdraw).toHaveBeenCalledWith(parseInt(userBalance, 10));
+            });
+        });
     });
 });
