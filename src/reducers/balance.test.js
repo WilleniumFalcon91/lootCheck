25de9ac17c4db52bc3df3e042aa3e3e60bf6af4d
William Elliott <wcelliott1991@gmail.com>
2017-12-04 19:26:21 -0500

    creates and passes deposit reducer and tests

--- src/reducers/balance.test.js
+++ src/reducers/balance.test.js
@@ -6,6 +6,14 @@ describe('balanceReducer', () => {
         const balance = 10;
 
         expect(balanceReducer(undefined, { type: constants.SET_BALANCE, balance }))
-        .toEqual(balance);
-    })
-})
+            .toEqual(balance);
+    });
+
+    it('deposits into the balance', () => {
+        const deposit = 10;
+        const initialState = 5;
+
+        expect(balanceReducer(initialState, { type: constants.DEPOSIT, deposit }))
+            .toEqual(initialState + deposit);
+    });
+});
