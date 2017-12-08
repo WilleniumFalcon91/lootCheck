7834d482dbb0070fe135d090e0d4b8c4ee2f4d42
William Elliott <wcelliott1991@gmail.com>
2017-12-08 10:34:31 -0500

    adds styling and credit

--- src/components/App.js
+++ src/components/App.js
@@ -10,6 +10,7 @@ const App = () =>  {
             <Wallet />
             <hr /> 
             <Loot />
+            <div>Powered by <a target='_blank' href="https://wwww.coindesk.com/price">Coindesk</a></div>
         </div>
     )
 }
